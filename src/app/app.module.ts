676485a5f63e92e57fd73d3d0d098759df14e53c
anas jaidi <anas.jaidi@icloud.com>
2023-05-05 22:51:59 +0100

    add basic structuer foldering and conf

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,20 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import logger from 'morgan';
+import helmet from 'helmet';
+import appConf from 'src/config/app.conf';
+import cors from 'cors';
+import { ConfigModule } from '@nestjs/config';
 
+// select settings for choosen mode
+const env = process.env.NODE_ENV || 'development';
+const conf = appConf(env)!;
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot()],
   controllers: [],
   providers: [],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(cors(), logger(), helmet()).forRoutes('*');
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
+
+// strat importing module
+
 import { AppModule } from './app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  await app.listen(3000, () => {
+    console.log(`* Environment : ${'env'}`);
+    console.log(`* Debugger : ${'debug' ? 'On' : 'Off'}`);
+    console.log(`* Running on http://localhost:${'port'} (CTRL + C to quit)`);
+  });
 }
 bootstrap();
