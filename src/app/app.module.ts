17bb001e6dd9cfd0ed3649fbae0edb9bee938802
anas jaidi <anas.jaidi@icloud.com>
2023-05-05 22:59:21 +0100

    adding app prefix url

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import logger from 'morgan';
 import helmet from 'helmet';
 import appConf from 'src/config/app.conf';
-import cors from 'cors';
 import { ConfigModule } from '@nestjs/config';
 
 // select settings for choosen mode
@@ -13,8 +12,8 @@ const conf = appConf(env)!;
   controllers: [],
   providers: [],
 })
-export class AppModule implements NestModule {
-  configure(consumer: MiddlewareConsumer) {
-    consumer.apply(cors(), logger(), helmet()).forRoutes('*');
-  }
+export class AppModule {
+  // configure(consumer: MiddlewareConsumer) {
+  //   consumer.apply(logger('dev'), helmet()).forRoutes('*');
+  // }
 }
--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,7 @@ import { AppModule } from './app/app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.setGlobalPrefix('/api/v1');
   await app.listen(3000, () => {
     console.log(`* Environment : ${'env'}`);
     console.log(`* Debugger : ${'debug' ? 'On' : 'Off'}`);
